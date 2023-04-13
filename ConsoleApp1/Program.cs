e77f8c89e4e32e1e1a729a3833a236c23ebb4936
sagar0840 <sagarkale55556@gmail.com>
2023-04-13 20:57:48 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr1 = new string[] { "Array", "swap", "Reverse", "fibonacci", "addition" };
+            string[] arr2 = new string[5] { "suraj","sagar","nagesh","0","0"};
+            
+            foreach (string i in arr1)
+            {
+                Console.WriteLine(i);
+            }
+            //Array.Sort(arr1);
+            Array.Copy(arr1, arr2,0 );
+            Array.Clear(arr1, 5, 2);
+            foreach (string i in arr2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+    }
+
